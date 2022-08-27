8b2b4e17fcc58fe6e5c3401b45f896cd367657e7
Sávio <77400555+SavioDesenvolver@users.noreply.github.com>
2022-08-27 18:18:02 -0300

    Atualizei alguns comandos e coloquei alguns
    é o titulo

---
+++ cmd/cmd/comando24.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "comando24",
+    code: `
+$description[1;report anything, disclosure in private without permission, scam, malicious links, threats, etc. everything sent in this channel will be sent to staff and deleted from that channel.]
+$description[2;Denunciem qualquer coisa, divulgação no privado sem permissão, scam, links maliciosos, ameaças, etc. Tudo que for enviado nesse canal será enviado para um canal privado da staff e deletado automaticamente.]
+ `}
---
+++ cmd/cmd/reboot.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name: "reboot",
+    code: ` $reboot[index.js]
+    $onlyForRoles[992932096840572988;Apenas ADMS tem permissão pra rodar esse comando!] ` } 
