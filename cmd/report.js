1353a32e5282ccd2825986bf5d4bef51cbb52638
Sávio <saviocns2002@gmail.com>
2022-11-27 09:07:19 -0300

    up new cmd

--- cmd/report.js
+++ cmd/report.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "$alwaysExecute",
     code: `
 $channelSendMessage[1013090568986951710;{newEmbed:{description:$message}{footer: By $username[$authorID]:$authorAvatar}]
-$channelSendMessage[1013188413702279258; <@$authorID> 👍]
+$channelSendMessage[960917624991133705; <@$authorID> 👍]
 $deletecommand
 $onlyIf[$channelID==1013090937850835005]
 
