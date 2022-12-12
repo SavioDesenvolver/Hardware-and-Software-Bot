ad7a39e7dc3462a2b2969b80cf239e7147d15891
Sávio <saviocns2002@gmail.com>
2022-12-12 19:31:59 -0300

    Create promo.js

---
+++ cmd/promo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "promo",
+    code: `
+$title[Promoção]
+$description[1;https://api.screenshotmachine.com?key=a2a170&url=$message&dimension=1024x1080]
+https://media.discordapp.net/attachments/1007451500173328494/1050906768957505637/unknown.png?width=960&height=279
+
+
+ `}
